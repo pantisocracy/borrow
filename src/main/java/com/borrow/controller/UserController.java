8b32c7d602fb693a0a1a42e0a11f54b471860f76
梁必聪 <liangbicong@epeizhen.com>
2016-03-18 13:28:11 +0800

    添加maven代码自动生成器

--- src/main/java/com/borrow/controller/UserController.java
+++ src/main/java/com/borrow/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.borrow.controller;
 
-import com.borrow.entity.User;
+import com.borrow.entity.UsersInfo;
 import com.borrow.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,34 +24,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
     @Transactional
     @RequestMapping("/save")
     @ResponseBody
     public void save() {
-        User user = new User();
-        user.setId(2L);
-        user.setUsername("liangbicong");
-        user.setPassword("liangbicong");
-        userService.save(user);
-
-        UserInfo userInfo = new UserInfo();
-        userInfo.setUsername("admin");
-        userInfo.setPassword("admin");
-        userService.save(userInfo);
+        UsersInfo usersInfo = new UsersInfo();
+        usersInfo.setId(1);
+        usersInfo.setAge(18);
+        usersInfo.setName("admin");
+        usersInfo.setSex(1);
+        usersInfo.setMobile(12112);
+        usersInfo.setSchoolId(212121);
+        usersInfo.setIdentity("1");
+        usersInfo.setSchoolId(1111);
+        usersInfo.setAddTime(new Date());
+        usersInfo.setApplyAccount(3232);
+        usersInfo.setType(1);
+        usersInfo.setUpdateTime(new Date());
+        userService.save(usersInfo);
     }
 
     @Transactional
     @RequestMapping("/find")
     @ResponseBody
     public String sayHello() {
-        //mybatis查询
-        List<User> userList = userService.getUserMapper();
-        System.out.println(userList.toString());
-        //分页查询
-        userList = userService.getUserMapperByPage();
-        System.out.println(userList.toString());
-        return userList.toString();
+        return null;
     }
     //跳转到jsp页面
     @RequestMapping("/jsp")
--- src/main/java/com/borrow/service/UserService.java
+++ src/main/java/com/borrow/service/UserService.java
@@ -1,43 +1,20 @@
 package com.borrow.service;
 
-import com.borrow.dao.mapper.UserMapper;
-import com.borrow.entity.User;
+import com.borrow.dao.mapper.UsersInfoMapper;
+import com.borrow.entity.UsersInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 /**
- * Created by Lipengfei on 2015/6/26.
+ * Created by lbc on 16/3/17.
  */
 @Service
 public class UserService {
 
     @Autowired
-    private UserMapper userMapper;
+    private UsersInfoMapper usersInfoMapper;
 
-    @Autowired
-    private UserInfoMapper userInfoMapper;
-
-    public List<User> getUserMapper() {
-        List<User> userList = userMapper.findAll();
-        return userList;
-    }
-
-    public List<User> getUserMapperByPage() {
-        PageRequest pageRequest = new PageRequest(0, 10);
-        Page<User> page = userMapper.findAllByPage(pageRequest);
-        List<User> list = page.getContent();
-        return list;
-    }
-    public void save(User user) {
-        userMapper.save(user);
-
-    }
-
-    public void save(UserInfo userInfo){
-        userInfoMapper.insert(userInfo);
+    public void save(UsersInfo usersInfo){
+        usersInfoMapper.insert(usersInfo);
     }
 }
